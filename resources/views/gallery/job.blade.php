b694a1294be5c6653f6345d3baec0c7f9188e798
MW5 <m.wojcik1988@gmail.com>
2016-10-01 19:51:45 +0200

    some updates

--- resources/views/gallery/job.blade.php
+++ resources/views/gallery/job.blade.php
@@ -36,7 +36,41 @@
     @section('jobs_gallery')
     <div class='container-fluid job_pics_container'>
         <div class='container'>
-            <p> HERE SOME STUFF</p>
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    @foreach($job_pics as $pic)
+                        @if($job->count==0)
+                            <div>NIMA ZDJECIOW</div>
+                        @elseif($pic->id == 0)
+                            <div class="item active">
+                                <img src="{{$pic->pic_src}}" alt="picture {{pic->id}}">
+                            </div>
+                        @else
+                            <div class="item">
+                                <img src="{{$pic->pic_src}}" alt="picture {{pic->id}}">
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+              </div>
         </div>
     </div>
     @stop
